abcdf0333bb22d7936bf36829a9d9d585d04596d
Tinghui <tinghui.zhang3@gmail.com>
2014-11-23 13:12:21 +0800

    Add sh file

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ iOS_Icons_Maker
 ===============
 
 A script for make iOS app icon files
+
+
+####Usage
+
+1. download the .sh file
+2. run it in terminal: 
+```
+$ sh path/to/iOS_Icons_Maker.sh path/to/sourceImage.png
+```
---
+++ iOS_Icons_Maker.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#usage: sh path/to/iOS_Icons_Maker.sh path/to/sourceImage.png
+
+ITUNES_ARTWORK="$1"
+FOLDER=$(dirname "$ITUNES_ARTWORK")
+
+sips -z 29 29 "$ITUNES_ARTWORK" --out "${FOLDER}/Icon-Small.png"
+sips -z 58 58 "$ITUNES_ARTWORK" --out "${FOLDER}/Icon-Small@2x.png"
+sips -z 87 87 "$ITUNES_ARTWORK" --out "${FOLDER}/Icon-Small@3x.png"
+sips -z 40 40 "$ITUNES_ARTWORK" --out "${FOLDER}/Icon-40.png"
+sips -z 80 80 "$ITUNES_ARTWORK" --out "${FOLDER}/Icon-40@2x.png"
+sips -z 120 120 "$ITUNES_ARTWORK" --out "${FOLDER}/Icon-40@3x.png"
+sips -z 57 57 "$ITUNES_ARTWORK" --out "${FOLDER}/Icon.png"
+sips -z 114 114 "$ITUNES_ARTWORK" --out "${FOLDER}/Icon@2x.png"
+sips -z 120 120 "$ITUNES_ARTWORK" --out "${FOLDER}/Icon-60@2x.png"
+sips -z 180 180 "$ITUNES_ARTWORK" --out "${FOLDER}/Icon-60@3x.png"
+sips -z 76 76 "$ITUNES_ARTWORK" --out "${FOLDER}/Icon-76.png"
+sips -z 152 152 "$ITUNES_ARTWORK" --out "${FOLDER}/Icon-76@2x.png"
